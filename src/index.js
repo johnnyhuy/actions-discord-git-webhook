187506c2b1bb12a6b2fbae43a182fc069a0becc2
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2021-07-06 12:01:49 +1000

    feat: Toggle links and use webhook URL

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 const analysis = require('../src/analysis.js');
 const webhook = require('../src/discord.js');
-
+https://discord.com/api/webhooks/775469717153513472/vkc8qka9AJ8suTqN4hAJvSB855yHAbA1Pzx0dyDjVLa62HSM--7y5aVWli9NDzzwqrFh
 async function run() {
 	const payload = github.context.payload;
 	const repository = payload.repository.full_name;
@@ -20,11 +20,20 @@ async function run() {
 		return;
 	}
 
-    const id = core.getInput("id");
-    const token = core.getInput("token");
+	const webhookUrl = core.getInput("webhook_url").split('/');
+
+	if (!!webhookUrl) {
+		const id = webhookUrl[5];
+    	const token = webhookUrl[6];
+	} else {
+		const id = core.getInput("id");
+    	const token = core.getInput("token");
+	}
+
+	const links = !!core.getInput("links") ? core.getInput("links") : false;
 
 	analysis.start(isSkipped(payload.head_commit)).then((report) => {
-        webhook.send(id, token, repository, branch, payload.compare, commits, size, report).catch(err => core.setFailed(err.message));
+        webhook.send(id, token, repository, branch, payload.compare, commits, size, report, links).catch(err => core.setFailed(err.message));
     }, err => core.setFailed(err));
 }
 
