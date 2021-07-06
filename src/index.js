1bf479597a95a54d9b485dccf942d059753141d7
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2021-07-06 23:25:44 +1000

    fix: Try get ID and token properly

--- src/index.js
+++ src/index.js
@@ -1,45 +1,56 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-const webhook = require('../src/discord.js');
+const webhook = require("../src/discord.js");
 
 async function run() {
-	const payload = github.context.payload;
-	const repository = payload.repository.full_name;
-	const commits = payload.commits;
-	const size = commits.length;
-	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1];
+  const payload = github.context.payload;
+  const repository = payload.repository.full_name;
+  const commits = payload.commits;
+  const size = commits.length;
+  const branch = payload.ref.split("/")[payload.ref.split("/").length - 1];
 
-	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`);
-	console.log(`Received ${commits.length}/${size} commits...`);
+  console.log(`Received payload ${JSON.stringify(payload, null, 2)}`);
+  console.log(`Received ${commits.length}/${size} commits...`);
 
-	if (commits.length === 0) {
-        // This was likely a "--tags" push.
-        console.log(`Aborting analysis, found no commits.`);
-		return;
-	}
+  if (commits.length === 0) {
+    // This was likely a "--tags" push.
+    console.log(`Aborting analysis, found no commits.`);
+    return;
+  }
 
-	const webhookUrl = core.getInput("webhook_url");
-	const links = core.getInput("links")
+  const webhookUrl = core.getInput("webhook_url");
+  const links = core.getInput("links");
+  let id = core.getInput("id");
+  let token = core.getInput("token");
 
-	if (webhookUrl !== '') {
-		const url = webhookUrl.split('/');
-		const id = url[5];
-    	const token = url[6];
-	} else {
-		const id = core.getInput("id");
-    	const token = core.getInput("token");
-	}
+  if (webhookUrl !== "") {
+    const url = webhookUrl.split("/");
+    id = url[5];
+    token = url[6];
+  }
 
-	webhook.send(id, token, repository, branch, payload.compare, commits, size, report, links).catch(err => core.setFailed(err.message));
+  webhook
+    .send(
+      id,
+      token,
+      repository,
+      branch,
+      payload.compare,
+      commits,
+      size,
+      report,
+      links
+    )
+    .catch((err) => core.setFailed(err.message));
 }
 
 try {
-	run();
+  run();
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
 
 function isSkipped(commit) {
-	return commit.message.toLowerCase().includes("[skip]");
+  return commit.message.toLowerCase().includes("[skip]");
 }
