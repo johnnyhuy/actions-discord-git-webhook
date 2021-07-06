bf95784f9fe287f1f3a2ff7c6e8fef41457fbad6
Johnny Huynh <info@johnnyhuy.com>
2021-07-06 12:09:38 +1000

    fix: Woops

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 const analysis = require('../src/analysis.js');
 const webhook = require('../src/discord.js');
-https://discord.com/api/webhooks/775469717153513472/vkc8qka9AJ8suTqN4hAJvSB855yHAbA1Pzx0dyDjVLa62HSM--7y5aVWli9NDzzwqrFh
+
 async function run() {
 	const payload = github.context.payload;
 	const repository = payload.repository.full_name;
