36be781d52f87fe5936255bda15b84f09818f573
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2021-11-26 00:05:43 +1100

    fix: Resolve on no commits

--- tests/main.test.js
+++ tests/main.test.js
@@ -7,35 +7,6 @@ jest.mock("@actions/core");
 
 afterEach(() => {
   jest.clearAllMocks();
-})
-
-test("get webhook url", () => {
-  const validWebhooks = [
-    {
-      url: "https://discord.com/api/webhooks/123/secret_sauce-webhooktoken",
-      id: "123",
-      token: "secret_sauce-webhooktoken",
-    },
-    {
-      url: "discord.com/api/webhooks/123/secret_sauce-webhooktoken",
-      id: "123",
-      token: "secret_sauce-webhooktoken",
-    },
-  ];
-  const invalidWebhooks = [
-    "https://discord.com/api/webhooks/thisis-bad",
-    "asdf",
-  ];
-
-  for (const webhook of invalidWebhooks) {
-    expect(() => getWebhook(webhook)).toThrow(ValidationException);
-  }
-
-  for (const webhook of validWebhooks) {
-    const [id, token] = getWebhook(webhook.url);
-    expect(id).toBe(webhook.id);
-    expect(token).toBe(webhook.token);
-  }
 });
 
 test("send webhook", async () => {
@@ -45,7 +16,7 @@ test("send webhook", async () => {
       send: jest.fn().mockResolvedValue(null),
     };
   });
-  discord.Client = jest.fn()
+  discord.Client = jest.fn();
   discord.MessageEmbed = jest.fn(() => {
     return {
       setURL: jest.fn().mockReturnThis(),
@@ -54,7 +25,7 @@ test("send webhook", async () => {
       setDescription: jest.fn().mockReturnThis(),
       setTimestamp: jest.fn().mockReturnThis(),
     };
-  })
+  });
   discord.WebhookClient.prototype.send = jest.fn(() => {
     return {
       send: jest.fn().mockRejectedValue(new Error()),
@@ -63,7 +34,13 @@ test("send webhook", async () => {
   const payload = require("./mocks/github_webhook_full.json");
 
   // Act
-  const data = await webhooks.send("https://discord.com/api/webhooks/123321123321/blah", payload, false, false, "blue");
+  const data = await webhooks.send(
+    "https://discord.com/api/webhooks/123321123321/blah",
+    payload,
+    false,
+    false,
+    "blue"
+  );
 
   // Assert
   expect(data).toBeNull();
@@ -76,7 +53,7 @@ test("send failed webhook", async () => {
       send: jest.fn().mockRejectedValue(new Error()),
     };
   });
-  discord.Client = jest.fn()
+  discord.Client = jest.fn();
   discord.MessageEmbed = jest.fn(() => {
     return {
       setURL: jest.fn().mockReturnThis(),
@@ -85,7 +62,7 @@ test("send failed webhook", async () => {
       setDescription: jest.fn().mockReturnThis(),
       setTimestamp: jest.fn().mockReturnThis(),
     };
-  })
+  });
   discord.WebhookClient.prototype.send = jest.fn(() => {
     return {
       send: jest.fn().mockRejectedValue(new Error()),
@@ -95,7 +72,13 @@ test("send failed webhook", async () => {
 
   try {
     // Act
-    await webhooks.send("https://discord.com/api/webhooks/913404819069345793/MF4mRyPECq9h4KI64B-UomFCcF1fZ1Ka_EnsQ89Hs0e0iaRc3migPGYdirz8odcpt2Wv", payload, false, false, "blue");
+    await webhooks.send(
+      "https://discord.com/api/webhooks/913404819069345793/MF4mRyPECq9h4KI64B-UomFCcF1fZ1Ka_EnsQ89Hs0e0iaRc3migPGYdirz8odcpt2Wv",
+      payload,
+      false,
+      false,
+      "blue"
+    );
   } catch (error) {
     // Assert
     expect(error).toStrictEqual(new Error());
