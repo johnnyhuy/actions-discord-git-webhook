3977d87d491684401fc1f36f658225a15ea447bf
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2021-11-26 13:29:42 +1100

    fix: Backtick fix

--- src/webhooks.js
+++ src/webhooks.js
@@ -29,7 +29,7 @@ module.exports.send = (
 
   let embed = new discord.MessageEmbed()
     .setColor(color)
-    .setTitle(`⚡ ${size} ${count}\n📁`${repository}\`\n🌳 \`${branch}\``)
+    .setTitle(`⚡ ${size} ${count}\n📁\`${repository}\`\n🌳 \`${branch}\``)
     .setDescription(this.getChangeLog(payload, hideLinks, censorUsername))
     .setTimestamp(Date.parse(latest.timestamp));
 
